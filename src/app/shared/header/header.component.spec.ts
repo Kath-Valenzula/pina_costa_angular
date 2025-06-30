e302c68410209ae4f1b23a1a1cc399a890393697
Kath Stark <ka.valenzuelam@duocuc.cl>
2025-06-30 13:58:38 -0400

    ruta

--- src/app/shared/header/header.component.spec.ts
+++ src/app/shared/header/header.component.spec.ts
@@ -2,7 +2,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HeaderComponent } from './header.component';
-import { CartService } from 'src/app/services/cart.service';
+import { CartService } from '../../services/cart.service';
 import { of } from 'rxjs';
 
 describe('HeaderComponent', () => {
